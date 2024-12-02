ec6a804961e9486f728b3326ce0abeafa517c699
KRISHNAKUMAR-N-04 <krishnakumarnagarajan04@gmail.com>
2024-12-03 00:13:33 +0530

    Starting of portfolio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './Components/Navbar'
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      Hello
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () =>{
+    return(
+        <div className="Navbar-holder">
+            <ul>
+
+            </ul>
+        </div>
+    )
+}
+export default Navbar;
